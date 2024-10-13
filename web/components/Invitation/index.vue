4a35d39466ac1abc1cfbdfa4a0c75f0154615177
bangtx <bangtx@otani-trading.com>
2024-10-13 22:08:41 +0700

    init

--- web/components/Invitation/index.vue
+++ web/components/Invitation/index.vue
@@ -10,12 +10,12 @@
           p.align-center Tới dự bữa cơm thân mật và
           p.align-center chung vui cùng gia đình chúng tôi
           h3.pa-4.align-center vào 10h00 phút
-          h2.pa-4.align-center Ngày 21 tháng 01 năm 2024
-          p.align-center tức ngày 11 tháng 12 năm Quý Mão
+          h2.pa-4.align-center Ngày 27 tháng 10 năm 2024
+          p.align-center tức ngày 25 tháng 09 năm Giáp Thìn
           h2.pa-4.align-center Tại Gia Đình Nhà {{isGirl ? 'Gái' : 'Trai'}}
           h3.pa-4.align-center {{ isGirl ? 'Tổ dân phố số 2, Yên Định' : 'Xóm Quang Bắc, Hải Quang' }}, Hải Hậu, Nam Định
           p.align-center(style="font-family: 'Great Vibes', cursive; font-size: 22px") Sự hiện diện của quý vị
-          p.align-center(style="font-family: 'Great Vibes', cursive; font-size: 22px") là niền vinh hạnh cho gia đình chúng tôi
+          p.align-center(style="font-family: 'Great Vibes', cursive; font-size: 22px") là niềm vinh hạnh cho gia đình chúng tôi
     v-col(cols="0" md="3")
 </template>
 
